9746939afa19a27df7c1461257c7dab367929525
DonShanilka <shanilka799@gmail.com>
2025-12-30 21:33:45 +0530

    add movie images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,53 @@
-'use client';
+"use client";
 
-import { Play, Plus, Star, X } from 'lucide-react';
-import { useState, useEffect } from 'react';
-import Navbar from '../components/layout/Navbar';
+import { Play, Plus, Star, X } from "lucide-react";
+import { useState, useEffect } from "react";
+import Navbar from "../components/layout/Navbar";
 
 /* ================= HERO MOVIES ================= */
 const featuredMovies = [
   {
     id: 1,
-    title: 'Avatar',
-    genre: 'Sci-Fi / Adventure',
-    year: 2009,
-    duration: '162 min',
+    title: "Avatar Fire and Ash",
+    genre: "Sci-Fi / Adventure",
+    year: 2025,
+    duration: "200 min",
     rating: 8.2,
     image:
-    'https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,fl_progressive,q_auto,w_1024/682ccc891f4663001daf980d.jpg',
+      "https://4kwallpapers.com/images/wallpapers/jurassic-world-3840x2160-22719.jpg",
     video:
-      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-  },
-  {
-    id: 2,
-    title: 'Avatar: The Way of Water',
-    genre: 'Sci-Fi / Action',
-    year: 2022,
-    duration: '192 min',
-    rating: 7.9,
-    image:
-      'https://images.unsplash.com/photo-1517602302552-471fe67acf66?auto=format&fit=crop&w=1920&q=80',
-    video:
-      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+      "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
   },
 ];
 
 /* ================= MOVIE GRID ================= */
 const movieImages = [
-  'https://images.unsplash.com/photo-1524985069026-dd778a71c7b4',
-  'https://images.unsplash.com/photo-1517602302552-471fe67acf66',
-  'https://images.unsplash.com/photo-1535016120720-40c646be5580',
-  'https://images.unsplash.com/photo-1489599849927-2ee91cede3ba',
+  "https://images.herzindagi.info/her-zindagi-english/images/2025/06/13/article/image/How-to-Train-Your-Dragon-2025-Trivia-5-Fun-Facts-About-The-Film-1749804963493.webp",
+  "https://m.media-amazon.com/images/M/MV5BYjQxYWNiNzgtOTc2Yi00OGEwLTk5MjAtODdiZTk0ZDJlZGY4XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+  "https://multiplex.ua/images/34/59/3459a9298fc6b5a4a9cc4f2d7de63ecd.jpeg",
+  "https://static0.srcdn.com/wordpress/wp-content/uploads/2023/06/how-to-train-your-dragon-the-hidden-world-poster.jpeg",
+  "https://cdn.europosters.eu/image/1300/213147.jpg",
+  "https://media.themoviedb.org/t/p/w440_and_h660_face/aLVkiINlIeCkcZIzb7XHzPYgO6L.jpg",
+  "https://media.themoviedb.org/t/p/w600_and_h900_face/bR8ISy1O9XQxqiy0fQFw2BX72RQ.jpg",
+  "https://m.media-amazon.com/images/I/81i1QiaNMaL._AC_UF894,1000_QL80_.jpg",
+  "https://i.ebayimg.com/00/s/MTYwMFgxMDM1/z/sM8AAOSw3fZaOwmI/$_57.JPG?set_id=8800005007",
+  "https://resizing.flixster.com/ko-hEHMbiGu6zup-oQx6yuoSt4k=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p12009531_v_v10_ab.jpg",
+  "https://m.media-amazon.com/images/M/MV5BZDYxY2I1OGMtN2Y4MS00ZmU1LTgyNDAtODA0MzAyYjI0N2Y2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+  "https://m.media-amazon.com/images/S/pv-target-images/f0535dd61f56bddd6ee7f3bfb765645e45d78f373418ae37ee5103cf6eebbff0.jpg",
+  "https://m.media-amazon.com/images/I/81nLIF+eJ7L._AC_UF1000,1000_QL80_.jpg",
+  "https://m.media-amazon.com/images/I/91ejswAr0eL._AC_UF894,1000_QL80_.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStLVXEGRgSilajmqZ79UfkTy_hkDnKeod4k4u_7U-_-8wUlNBfjSVNx6cSH17gnQuDmYzqDQ&s=10",
 ];
 
 const movies = Array.from({ length: 20 }).map((_, i) => ({
   id: i + 1,
   title: `Movie ${i + 1}`,
   rating: (Math.random() * 2 + 7).toFixed(1),
-  image: `${movieImages[i % movieImages.length]}?auto=format&fit=crop&w=400&q=80`,
+  image: `${
+    movieImages[i % movieImages.length]
+  }?auto=format&fit=crop&w=400&q=80`,
   video:
-    'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
 }));
 
 /* ================= PAGE ================= */
@@ -67,135 +68,135 @@ export default function Home() {
   /* ESC KEY CLOSE */
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
-      if (e.key === 'Escape') closeModal();
+      if (e.key === "Escape") closeModal();
     };
-    window.addEventListener('keydown', handler);
-    return () => window.removeEventListener('keydown', handler);
+    window.addEventListener("keydown", handler);
+    return () => window.removeEventListener("keydown", handler);
   }, []);
 
   return (
-    <div className="bg-black min-h-screen text-white">
+    <div>
       <Navbar />
-
-      {/* ================= HERO ================= */}
-      {featuredMovies.map((movie) => (
-        <div key={movie.id} className="relative h-[85vh] pt-20 overflow-hidden">
+      <div className="bg-black min-h-screen text-white py-18">
+        {/* ================= HERO ================= */}
+        {featuredMovies.map((movie) => (
           <div
-            className="absolute inset-0 bg-cover bg-center scale-105"
-            style={{ backgroundImage: `url(${movie.image})` }}
+            key={movie.id}
+            className="relative h-[85vh] pt-20 overflow-hidden"
           >
-            <div className="absolute inset-0 bg-gradient-to-r from-black via-black/70 to-transparent" />
-            <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent" />
-          </div>
+            <div
+              className="absolute inset-0 bg-cover bg-center scale-105"
+              style={{ backgroundImage: `url(${movie.image})` }}
+            >
+              <div className="absolute inset-0 bg-gradient-to-r from-black via-black/70 to-transparent" />
+              <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent" />
+            </div>
 
-          <div className="relative h-full flex items-center px-12">
-            <div className="max-w-2xl">
-              <p className="text-yellow-400 mb-3 text-sm">{movie.genre}</p>
+            <div className="relative h-full flex items-center px-12">
+              <div className="max-w-2xl">
+                <p className="text-yellow-400 mb-3 text-sm">{movie.genre}</p>
 
-              <h1 className="text-6xl font-black mb-4">{movie.title}</h1>
+                <h1 className="text-6xl font-black mb-4">{movie.title}</h1>
 
-              <div className="flex gap-4 text-sm text-gray-300 mb-6">
-                <span>{movie.year}</span>
-                <span>•</span>
-                <span>{movie.duration}</span>
-                <span className="flex items-center gap-1 text-yellow-400">
-                  <Star size={14} fill="currentColor" />
-                  {movie.rating}
-                </span>
+                <div className="flex gap-4 text-sm text-gray-300 mb-6">
+                  <span>{movie.year}</span>
+                  <span>•</span>
+                  <span>{movie.duration}</span>
+                  <span className="flex items-center gap-1 text-yellow-400">
+                    <Star size={14} fill="currentColor" />
+                    {movie.rating}
+                  </span>
+                </div>
+
+                <button
+                  onClick={() => openModal(movie)}
+                  className="flex items-center gap-2 bg-yellow-400 text-black px-8 py-3 rounded-lg font-semibold hover:bg-yellow-500 transition"
+                >
+                  <Play className="w-5 h-5 fill-current" />
+                  Watch Now
+                </button>
               </div>
-
-              <button
-                onClick={() => openModal(movie)}
-                className="flex items-center gap-2 bg-yellow-400 text-black px-8 py-3 rounded-lg font-semibold hover:bg-yellow-500 transition"
-              >
-                <Play className="w-5 h-5 fill-current" />
-                Watch Now
-              </button>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
 
-      {/* ================= MOVIE GRID ================= */}
-      <section className="px-12 py-16">
-        <h2 className="text-3xl font-bold mb-8">Trending Movies</h2>
+        {/* ================= MOVIE GRID ================= */}
+        <section className="px-12 py-16">
+          <h2 className="text-3xl font-bold mb-8">Trending Movies</h2>
 
-        <div className="grid grid-cols-10 gap-4">
-          {movies.map((movie) => (
-            <div
-              key={movie.id}
-              onClick={() => openModal(movie)}
-              className="group relative cursor-pointer"
-            >
-              <img
-                src={movie.image}
-                className="h-60 w-full rounded-lg object-cover"
-              />
+          <div className="grid grid-cols-5 gap-4">
+            {movies.map((movie) => (
+              <div
+                key={movie.id}
+                onClick={() => openModal(movie)}
+                className="group relative cursor-pointer"
+              >
+                <img
+                  src={movie.image}
+                  className="h-96 w-full object-cover rounded-lg "
+                />
 
-              <div className="absolute inset-0 bg-black/70 opacity-0 group-hover:opacity-100 transition rounded-lg flex items-end p-3">
-                <div className="flex justify-between w-full">
-                  <span className="text-sm">{movie.title}</span>
-                  <Play className="text-yellow-400" />
+                <div className="absolute inset-0 bg-black/70 opacity-0 group-hover:opacity-100 transition rounded-lg flex items-end p-3">
+                  <div className="flex justify-between w-full">
+                    <span className="text-sm">{movie.title}</span>
+                    <Play className="text-yellow-400" />
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </section>
-
-      {/* ================= MODAL ================= */}
-      {open && selectedMovie && (
-        <div
-          onClick={closeModal}
-          className="fixed inset-0 bg-black/80 z-50 flex items-center justify-center animate-fadeIn"
-        >
-          <div
-            onClick={(e) => e.stopPropagation()}
-            className="relative w-[70%] bg-black rounded-xl overflow-hidden animate-scaleIn"
-          >
-            <button
-              onClick={closeModal}
-              className="absolute top-4 right-4 z-10 bg-black/70 p-2 rounded-full"
-            >
-              <X />
-            </button>
-
-            <video
-              controls
-              autoPlay
-              className="w-full h-[75vh] object-cover"
-            >
-              <source src={selectedMovie.video} type="video/mp4" />
-            </video>
+            ))}
           </div>
-        </div>
-      )}
+        </section>
 
-      {/* ================= ANIMATIONS ================= */}
-      <style jsx global>{`
-        @keyframes fadeIn {
-          from {
-            opacity: 0;
+        {/* ================= MODAL ================= */}
+        {open && selectedMovie && (
+          <div
+            onClick={closeModal}
+            className="fixed inset-0 bg-black/80 z-50 flex items-center justify-center animate-fadeIn"
+          >
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className="relative w-[70%] bg-black rounded-xl overflow-hidden animate-scaleIn"
+            >
+              <button
+                onClick={closeModal}
+                className="absolute top-4 right-4 z-10 bg-black/70 p-2 rounded-full"
+              >
+                <X />
+              </button>
+
+              <video controls autoPlay className="w-full h-[75vh] object-cover">
+                <source src={selectedMovie.video} type="video/mp4" />
+              </video>
+            </div>
+          </div>
+        )}
+
+        {/* ================= ANIMATIONS ================= */}
+        <style jsx global>{`
+          @keyframes fadeIn {
+            from {
+              opacity: 0;
+            }
+            to {
+              opacity: 1;
+            }
           }
-          to {
-            opacity: 1;
+          @keyframes scaleIn {
+            from {
+              transform: scale(0.9);
+            }
+            to {
+              transform: scale(1);
+            }
           }
-        }
-        @keyframes scaleIn {
-          from {
-            transform: scale(0.9);
+          .animate-fadeIn {
+            animation: fadeIn 0.3s ease;
           }
-          to {
-            transform: scale(1);
+          .animate-scaleIn {
+            animation: scaleIn 0.25s ease;
           }
-        }
-        .animate-fadeIn {
-          animation: fadeIn 0.3s ease;
-        }
-        .animate-scaleIn {
-          animation: scaleIn 0.25s ease;
-        }
-      `}</style>
+        `}</style>
+      </div>
     </div>
   );
 }
